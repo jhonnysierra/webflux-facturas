6474d19c2dcd81d56d369ab43c5bf7a8319fb9f9
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-02-23 14:11:26 -0500

    Update ImpFacturaService.java
    Se agrega funciones para imprimir logs

--- src/main/java/com/example/webflux/facturas/service/impl/ImpFacturaService.java
+++ src/main/java/com/example/webflux/facturas/service/impl/ImpFacturaService.java
@@ -3,12 +3,18 @@ package com.example.webflux.facturas.service.impl;
 import com.example.webflux.facturas.model.Factura;
 import com.example.webflux.facturas.model.Producto;
 import com.example.webflux.facturas.service.IFacturaService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+
 
 @Service
 public class ImpFacturaService implements IFacturaService {
@@ -16,8 +22,9 @@ public class ImpFacturaService implements IFacturaService {
     /**
      * Se crea un web client para realizar el llamado a otra API REST que se esta corriendo en el sistema
      */
-    private final WebClient productos = WebClient.create("http://localhost:8091/productos");
+    private final WebClient productos = WebClient.create("http://localhost:8091/producto");
 
+    private static final Logger logger = LoggerFactory.getLogger(ImpFacturaService.class);
     /**
      * Se crea un delay para que el flujo tarde 3 segundos en responder
      */
@@ -30,16 +37,28 @@ public class ImpFacturaService implements IFacturaService {
     @Override
     public Flux<Factura> listarFacturas() {
         facturas.subscribe(System.out::println);//x->System.out.println(X)
-/*
-        Flux<Producto> productosFlux = productos.get().retrieve().bodyToFlux(Producto.class);
-        productosFlux.subscribe(System.out::println);
-*/
+        /*
+        logger.info("Se inicia el log listarFacturas");
+        facturas.subscribe(response -> logger.info("Consulta de facturas exitosa {}", response));
+
+         productos.get()
+                 .retrieve()
+                 .bodyToFlux(Producto.class)
+                 //.doOnError(error -> logger.error("Mensaje de error API {} ", error.getMessage()))
+                 .onErrorResume(error -> {
+                     logger.error("Ocurrió un error {} ", error.getMessage());
+                     return Mono.error(error);
+                 })
+                 .subscribe();
+        */
+        //productosFlux.subscribe(System.out::println);
+
         return facturas;
     }
 
     /**
      * single-> funciona siempre y cuando no hayan valores duplicados
-     * next() -> Encuentr la primera ocurrencia del dato
+     * next() -> Encuentra la primera ocurrencia del dato
      * @param id
      * @return
      */
@@ -47,7 +66,8 @@ public class ImpFacturaService implements IFacturaService {
     public Mono<Factura> buscarFacturaId(int id) {
         Mono<Factura> factura = facturas
                 .filter(element-> element.getId()==id)
-                .next();
+                .next()
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
         return factura;
     }
 }
